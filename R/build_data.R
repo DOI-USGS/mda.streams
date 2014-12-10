8d013ad76e33f76d35fec71543dc6b1a62b9f7e3
unknown <ldecicco@usgs.gov>
2014-12-10 16:53:04 -0600

    Here's a way to find your data no matter what OS. I would move int_data.R to inst/extdata.  I would also just put that list in data as a .RData file, and skip this all together.

--- R/build_data.R
+++ R/build_data.R
@@ -1,4 +1,5 @@
 # runs on package build. Need to fix for windows...
 if (file.exists('~/Documents/R/mda.streams/inst/init_data.R')){
-  source('~/Documents/R/mda.streams/inst/init_data.R')
+  filePath <- system.file("inst", "int_data.R", package="mda.streams")
+  source(filePath)
 }
