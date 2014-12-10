21c25ab0151a41861f14ae13b9309ca9e0b21d06
Jordan S Read <jread@usgs.gov>
2014-12-10 16:03:14 -0600

    initial commit of function

---
+++ R/get_watershed_WFS.R
@@ -0,0 +1,17 @@
+#'@title get WFS from site
+#'@description get a WFS url from a site that has a 'watershed'
+#'
+#'@param site a site identifier
+#'@return a character string for WFS url or NULL if watershed doesn't exist for site, or site doesn't exist.
+#'
+#'@examples
+#'site <- "nwis_01018035"
+#'get_watershed_WFS(site)
+#'
+#'@import sbtools
+#'@export
+get_watershed_WFS = function(site){
+  
+  
+  return(WFS_url)
+}
