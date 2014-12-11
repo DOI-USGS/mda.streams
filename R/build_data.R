4ed9666d0bf0fcc33567eecae8f7437647fc2e11
Jordan S Read <jread@usgs.gov>
2014-12-10 19:09:42 -0600

    reverting

--- R/build_data.R
+++ R/build_data.R
@@ -1,5 +1,4 @@
 # runs on package build. Need to fix for windows...
 if (file.exists('~/Documents/R/mda.streams/inst/init_data.R')){
-  filePath <- system.file("inst", "int_data.R", package="mda.streams")
-  source(filePath)
+  source('~/Documents/R/mda.streams/inst/init_data.R')
 }
