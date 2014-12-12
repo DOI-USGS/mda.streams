ed6fbca67f9e63b81dff09d79db23bccb685d6b7
Jordan S Read <jread@usgs.gov>
2014-12-12 14:15:08 -0600

    broken out helper functions

--- R/get_watershed_WFS.R
+++ R/get_watershed_WFS.R
@@ -5,7 +5,7 @@
 #'@param session arguments passed to sbtools \code{query_item_identifier} for authentication. 
 #'Not needed if parent of sites is public 
 #'@return a character string for WFS url or NULL if watershed doesn't exist for site, or site doesn't exist.
-#'
+#'@seealso \code{\link{get_watershed_WMS}}
 #'@examples
 #'site <- "nwis_01018035"
 #'get_watershed_WFS(site)
@@ -19,23 +19,34 @@
 #'@export
 get_watershed_WFS = function(site, session = NULL){
   
+  watershed_item <- get_watershed_item(site, session)
+ 
+  WFS_url <- match_url_distro(watershed_item, "ScienceBase WFS Service")
+  return(WFS_url)
+}
+
+get_watershed_item = function(site, session){
   # session not needed since items are public
   identifier <- query_item_identifier(scheme = 'mda_streams', type = 'watershed', key = site,  session = session)
   
   watershed_id <- identifier$id
   
   watershed_item <- item_get(id = watershed_id, session = session)
-  watershed_item[["distributionLinks"]]
   
-  num_links <- length(watershed_item[["distributionLinks"]])
+  return(watershed_item)
+}
+match_url_distro = function(item, title_to_match){
   
-  WFS_url = NULL
+  num_links <- length(item[["distributionLinks"]])
+  
+  url = NULL
   for (i in seq_len(num_links)){
-    if (watershed_item[["distributionLinks"]][[i]][['title']] == "ScienceBase WFS Service"){
-      WFS_url <- watershed_item[["distributionLinks"]][[i]][['uri']]
+    if (item[["distributionLinks"]][[i]][['title']] == title_to_match){
+      url <- item[["distributionLinks"]][[i]][['uri']]
       break
     }
   }
   
-  return(WFS_url)
+  return(url)
+  
 }
