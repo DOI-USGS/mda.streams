7d9963b460aa2b012db78d6d79c6b1b4fbf15f2e
Jordan S Read <jread@usgs.gov>
2015-01-12 14:10:04 -0600

    forgot to add this helper file

---
+++ R/mda_helpers.R
@@ -0,0 +1,19 @@
+make_ts_variable <- function(variable){
+  ts_prefix <- get_ts_prefix()
+  ts_variable = paste0(ts_prefix, variable)
+  return(ts_variable)
+}
+
+get_ts_prefix <- function(){
+  ts_prefix = 'ts_'
+  return(ts_prefix)
+}
+
+get_ts_extension <- function(){
+  ts_extension = '.tsv'
+  return(ts_extension)
+}
+
+get_ts_delim <- function(){
+  return('\t')
+}
