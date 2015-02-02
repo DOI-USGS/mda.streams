db0addc1715aa6a03259faca58605770e5789b64
Jordan S Read <jread@usgs.gov>
2015-02-02 15:42:33 -0600

    updated for sbtools set_endpoint

--- R/create_site.R
+++ R/create_site.R
@@ -2,33 +2,38 @@
 #'
 #'
 #'
-#'@param site Local site ID (e.g. NWIS_09238475)
+#'@param site Local site ID (e.g. nwis_09238475)
 #'@param session Session object from \link{authenticate_sb}
+#'@param skip_exists boolean for skip creation if item already exists.
 #'
 #'@author Corinna Gries
 #'
 #'
 #'@import sbtools
 #'@export
-create_site <- function(site, session){
+create_site <- function(site, session, replace_existing = FALSE){
   
   #check that it doesn't already exits
   item = query_item_identifier(scheme='mda_streams',type="site_root", 
                                key=site, session=session)
   
   if(nrow(item) > 0){
-    stop('This site already exists')
+    if (replace_existing){
+      item_rm(id, session)
+    } else {
+      stop('This site already exists')
+    }
   }	
   
-  #create site in SB 5487139fe4b02acb4f0c8110
-  ts_item = item_create(parent_id="5487139fe4b02acb4f0c8110", 
+  project_id <- get_project_id()
+  item = item_create(parent_id = project_id, 
                         title=site, session=session)
   
   #tag item with our special identifier
-  item_update_identifier(ts_item, scheme="mda_stream", type="site_root",
+  item_update_identifier(item, scheme="mda_streams", type="site_root",
                          key=site, session=session)
   
   
-  return(ts_item)  
+  return(item)  
   
 }
