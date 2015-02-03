6122cb7d3bc2c5b45b3005d73f3ef0d6dd5c12f4
Jordan S Read <jread@usgs.gov>
2015-02-03 11:24:40 -0600

    match sites w/ child key arg

--- R/get_sites.R
+++ R/get_sites.R
@@ -1,7 +1,8 @@
 #'@title get site IDS from sciencebase
 #'@description collects all "site_root" titles from sciencebase, returns as a character vector
 #'
-#'@param session arguments passed to sbtools \code{query_item_identifier} for authentication. 
+#'@param with_child_key limit sites to those with children matching the specified key
+#'@param session arguments passed to sbtools \code{\link{query_item_identifier}} for authentication. 
 #'Not needed if parent of sites is public
 #'@param limit numeric. Max number of sites to return. Default 10000
 #'@return a character vector of "site_root" titles (keys)
@@ -14,13 +15,37 @@
 #'# get sites if they are private
 #'session <- authenticate_sb(username, password)
 #'get_sites(session = session, limit = 10)
+#'# get sites with water temperature
+#'get_sites(with_child_key = 'wtr')
 #'}
+#'@import jsonlite httr
 #'@export
-get_sites <- function(session = NULL, limit = 10000){
+get_sites <- function(with_child_key = NULL, session = NULL, limit = 10000){
   
-  # session not needed since items are public
-  identifiers <- query_item_identifier(scheme = 'mda_streams', type = 'site_root', session = session, limit = limit)
+  scheme <- 'mda_streams'
+  type <- 'site_root'
+
+  #get the superset of sites, which is used for both options
+  identifiers <- query_item_identifier(scheme = scheme, type = type, session = session, limit = limit)
   
-  sites <- identifiers$title
+  if (is.null(with_child_key)){
+    sites <- identifiers$title
+  } else {
+    
+    filter_items = list('scheme'=scheme, 'type'= with_child_key)
+    filter = paste0('itemIdentifier=', toJSON(filter_items, auto_unbox=TRUE))
+    
+    query = list('filter' = filter, 'max' = limit, 'format' = 'json', 'fields' = 'parentId')
+    child_ids <- query_items(query, session) 
+    response <- content(child_ids, 'parsed')
+
+    sites = vector(mode = 'character', length = length(response$items))
+    for(i in 1:length(response$items)){
+      parent_id <- response$items[[i]]$parentId
+      # match to the superset
+      sites[i] <- identifiers$title[identifiers$id == parent_id]
+    }
+  }
+
   return(sites)
 }
