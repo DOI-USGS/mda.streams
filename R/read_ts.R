080c9e7e3b56d57923e137b831b5fdcf0f57aeb7
Jordan S Read <jread@usgs.gov>
2015-02-03 07:51:39 -0600

    stooopid dates

--- R/read_ts.R
+++ R/read_ts.R
@@ -20,6 +20,6 @@ read_ts = function(file_handle){
   
   ts_delim <- get_ts_delim()
   df <- read.table(file_handle, header = TRUE, sep = ts_delim)
-  df[, 1] <- as.POSIXct(df[, 1])
+  df[, 1] <- as.POSIXct(df[, 1], tz = 'GMT')
   return(df)
 }
