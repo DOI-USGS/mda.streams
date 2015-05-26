69be99d3f1f449f183b192a81230c69c68131853
Alison Appling <aappling@usgs.gov>
2015-05-26 13:00:50 -0500

    get/set scheme

---
+++ R/AAA.R
@@ -0,0 +1,12 @@
+#' Define a package environment for storing data specific to a project during an
+#' R session
+#' 
+#' \code{scheme} - string identifying the ScienceBase scheme where this SB project
+#'   resides, accessed by \code{\link{set_scheme}} and \code{\link{get_scheme}}.
+#' @return the package environment
+define_pkg_env <- function() {
+  pkg.env <- new.env()
+  pkg.env$scheme <- "mda_streams"
+  return(pkg.env)
+}
+pkg.env <- define_pkg_env()
