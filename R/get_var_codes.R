3dee351258a6479aba18cd7f292964dfbf7043a1
Alison Appling <aappling@usgs.gov>
2015-06-01 10:19:56 -0500

    add drop option to get_var_codes

--- R/get_var_codes.R
+++ R/get_var_codes.R
@@ -7,10 +7,12 @@
 #'   these options may be selected: 
 #'   \code{c("shortname","p_code","sm_name","longname")} (where \code{shortname}
 #'   is also the rowname)
+#' @param drop logical. In data.frame subsetting, should a single column be
+#'   dropped to a vector or a single row be dropped to a list?
 #' @return vector or data.frame of the requested variables and outnames. 
 #'   \code{shortname} is the name used in mda.streams. \code{p_code} is the 
-#'   corresponding code in the NWIS or NLDAS databases. \code{sm_name} is the
-#'   name used in streamMetabolizer. \code{longtime} is the human-readable
+#'   corresponding code in the NWIS or NLDAS databases. \code{sm_name} is the 
+#'   name used in streamMetabolizer. \code{longtime} is the human-readable 
 #'   description.
 #' @examples
 #' get_var_codes()
@@ -18,7 +20,7 @@
 #' get_var_codes('disch', 'p_code')
 #' get_var_codes('baro', c('longname','p_code'))
 #' @export
-get_var_codes <- function(shortname, out=c("p_code","sm_name","longname")){
+get_var_codes <- function(shortname, out=c("p_code","sm_name","longname"), drop=TRUE){
   
   codes <- matrix(
     data=
@@ -37,11 +39,11 @@ get_var_codes <- function(shortname, out=c("p_code","sm_name","longname")){
     do({rownames(.) <- .$shortname; .})
   
   if (missing(shortname))
-    return(codes[,out])
+    return(codes[ , out, drop])
   
   if (!shortname %in% rownames(codes))
     stop (shortname, ' not found in ', paste(names(codes), collapse = ', '))
   
-  return(codes[shortname, out])
+  return(codes[shortname, out, drop])
   
 }
