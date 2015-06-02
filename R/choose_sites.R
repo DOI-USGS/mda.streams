f05f4eaf49db6bb031925f0a74d6505a09a4072e
Alison Appling <aappling@usgs.gov>
2015-06-02 14:54:14 -0500

    closes issue #76

--- R/choose_sites.R
+++ R/choose_sites.R
@@ -12,15 +12,7 @@
 #' choose_sites()
 #' }
 choose_sites <- function(required_vars=c("disch","doobs","wtr")) {
-  required_vars <- make_ts_name(required_vars)
-  var_sites <- lapply(required_vars, function(var) { 
-    get_sites(with_child_key=var)
-  })
-  good_sites <- var_sites[[1]]
-  if(length(required_vars) > 1) {
-    for(i in 2:length(var_sites)) {
-      good_sites <- intersect(good_sites, var_sites[[i]])
-    }
-  }
-  good_sites
+  # this should soon be an intersection of several calls to 
+  # find_sites(c("light.nldas","light.model"), logic="any"), etc.
+  find_sites(with_timeseries = required_vars, logic="all")
 }
