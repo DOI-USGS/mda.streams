e5e8a64dad23f994912a26774dca00ceb46a7e84
Alison Appling <aappling@usgs.gov>
2015-06-15 12:06:27 -0500

    fix locate_item suite

--- R/locate_item.R
+++ R/locate_item.R
@@ -1,10 +1,27 @@
 #' Find a folder or other item on ScienceBase
 #' 
-#' @param key the item key, probably identical to the folder name
-#' @param type the item type, probably either root or site_root
-#' @param format character indicating whether the folder should be returned as
+#' locate_item is case insensitive. This function is only guaranteed to return 
+#' the first match; others may exist and may or may not be returned.
+#' 
+#' @param key the item key, probably identical to the folder name. Examples are 
+#'   c("nwis_02322688", "project", "presentations", "proposals", "publications",
+#'   "sites")
+#' @param type the item type, probably either root or site_root. Examples are 
+#'   c("site_root","root")
+#' @param format character indicating whether the folder should be returned as 
 #'   an ID or as a full URL
-#' @param ... args passed to \code{\link[sbtools]{session_check_reauth}}
+#' @param by character indicating how to search for the item: using tags ("tag",
+#'   the default and recommended option), by scanning the parent directory for 
+#'   the desired title ("dir"), or both in combination ("either")?
+#' @param parent the ScienceBase ID of the parent whose children to search if 
+#'   \code{by \%in\% c("dir","either")}
+#' @param title the title to seek among the children if \code{by \%in\% 
+#'   c("dir","either")}
+#' @param limit number of items to return, as in 
+#'   \code{\link[sbtools]{query_item_identifier}} or 
+#'   \code{\link[sbtools]{item_list_children}}
+#' @import dplyr
+#' @import sbtools
 #' @examples 
 #' \dontrun{
 #' mda.streams:::locate_item(key="sites", type="root")
@@ -12,55 +29,50 @@
 #'   type="root", format="folder_url")
 #' mda.streams:::locate_item(key="nwis_02322688", type="site_root")
 #' }
-locate_item <- function(key=c("nwis_02322688", "project", "presentations", "proposals", "publications", "sites"), 
-                        type=c("site_root","root"), format=c("id","item_url","folder_url"), ...) {
+locate_item <- function(key, type, format=c("id","item_url","folder_url"), 
+                        by=c("tag","dir","either"), parent, title, limit=5000) {
   
   # process arguments
   format <- match.arg(format) # only one format per call to locate_item
-  query_args <- data.frame(key=key, type=type, stringsAsFactors=FALSE)
-  session_check_reauth(...)
-  
+  by <- match.arg(by)
+  query_args <- 
+    switch(
+      by,
+      tag=data.frame(key=key, type=type, stringsAsFactors=FALSE),
+      dir=data.frame(parent=parent, title=title, stringsAsFactors=FALSE),
+      either=data.frame(key=key, type=type, parent=parent, title=title, stringsAsFactors=FALSE)
+    ) %>%
+    lapply(tolower) %>%
+    as.data.frame(stringsAsFactors=FALSE)
+
   # run the query or queries
   sapply(1:nrow(query_args), function(argnum) {
     
-    # ask ScienceBase for the item
-    item <- query_item_identifier(scheme = get_scheme(), type = query_args$type[argnum], key = query_args$key[argnum])
+    # query by tag
+    item <- data.frame()
+    if(by %in% c("tag","either")) {
+      item <- query_item_identifier(scheme = get_scheme(), type = query_args$type[argnum], key = query_args$key[argnum], limit=limit)
+    }
+    
+    # query by dir
+    if(by == "dir" || (isTRUE(nrow(item)==0) && by == "either")) {
+      # find all children of the specified parent. for finding a site, it'd be
+      # more efficient to only list children once rather than every iteration of
+      # the sapply, but this should be a rare case.
+      kids <- item_list_children(query_args$parent[argnum], limit=limit)
+      itemnum <- NA
+      for(i in seq_len(nrow(kids))) {
+        kid_title <- tolower(item_get(kids[i,"id"])$title)
+        if(kid_title == query_args$title[argnum]) {
+          itemnum <- i
+          break
+        }
+      }
+      item <- if(!is.na(itemnum)) kids[itemnum,,drop=FALSE] else data.frame()
+    }    
     
     # reformat the result to our liking
     format_item(item, format)
-  })
-}
-
-#' Locate items using the folder structure rather than tags
-#' 
-#' This function is useful for ScienceBase directory maintenance, because it
-#' helps locate items that have no tags along with those that do. Items without
-#' tags are created on post_ts operations that fail partway through.
-#' 
-#' @param parent the ScienceBase ID of the parent whose children to search
-#' @param title the title to seek among the children
-#' @return an id, formatted as requested
-#' @keywords internal
-locate_item_by_dir <- function(parent, title, format=c("id","item_url","folder_url"), ...) {
-  # process arguments
-  format <- match.arg(format) # only one format per call to locate_item
-  query_args <- data.frame(parent=parent, title=title, stringsAsFactors=FALSE)
-  session_check_reauth(...)
-  
-  # run the query or queries
-  sapply(seq_len(nrow(query_args)), function(argnum) {
-    one_parent <- query_args$parent[argnum]
-    one_title <- query_args$title[argnum]
-    
-    # find all children of the specified parent
-    kids <- item_list_children(one_parent)
-    
-    # orphans are kids with parents and titles but not necessarily identity tags.
-    is_orphan <- sapply(kids$id, function(kid) item_get(kid)$title == one_title)
-    orphans <- if(length(is_orphan) > 0) kids[is_orphan,] else data.frame()
-    
-    # return properly formatted
-    format_item(orphans, format)
   })
 }
 
@@ -94,17 +106,17 @@ format_item <- function(item, format) {
 #' locate_folder("publications", format="url")
 #' testthat::expect_error(locate_folder("cvs", format="url"))
 #' }
-locate_folder <- function(folder=c("project","presentations","proposals","publications","sites"), format=c("id","url"), ...) {
+locate_folder <- function(folder=c("project","presentations","proposals","publications","sites"), 
+                          format=c("id","url"), by=c("tag","dir","either"), limit=5000) {
+  folder <- tolower(folder)
   folder <- match.arg(folder)
   format <- switch(match.arg(format), id="id", url="folder_url")
-  locate_item(key=folder, type="root", format=format, ...)
+  locate_item(key=folder, type="root", parent=locate_folder("project"), title=folder, by=by, format=format, limit=limit)
 }
 
 #' Find a site folder on ScienceBase
 #' 
 #' @param site_name the site ID, e.g. "nwis_02322688", whose folder you want
-#' @param by character indicating how to search for the item: using tags
-#'   (the default) or by scanning the parent directory for the desired title?
 #' @inheritParams locate_item
 #' @export
 #' @examples 
@@ -114,22 +126,19 @@ locate_folder <- function(folder=c("project","presentations","proposals","public
 #' locate_site("nwis_notasite", format="url")
 #' testthat::expect_error(locate_site("notasite", format="url"))
 #' }
-locate_site <- function(site_name, format=c("id","url"), by=c("tag","dir"), ...) {
+locate_site <- function(site_name, format=c("id","url"), by=c("tag","dir","either"), limit=5000) {
   by <- match.arg(by)
   site_name <- make_site_name(parse_site_name(site_name)) # check that the site name is reasonably valid
   format <- switch(match.arg(format), id="id", url="folder_url")
-  switch(
-    by,
-    "tag"=locate_item(key=site_name, type="site_root", format=format, ...),
-    "dir"=locate_item_by_dir(parent=locate_folder("sites"), title=site_name, format=format, ...))
+  locate_item(key=site_name, type="site_root", parent=locate_folder("sites"), title=site_name, by=by, format=format, limit=limit)
 }
 
 #' Find a timeseries item on ScienceBase
 #' 
-#' @param var_src the variable name, e.g., "doobs_nwis"
-#' @param site_name the site ID, e.g. "nwis_02322688", whose folder you want
-#' @param by character indicating how to search for the item: using tags
-#'   (the default) or by scanning the parent directory for the desired title?
+#' @param var_src the variable name, e.g., "doobs_nwis", for which you want the 
+#'   timeseries
+#' @param site_name the site ID, e.g. "nwis_02322688", whose folder you want to
+#'   look in
 #' @inheritParams locate_item
 #' @export
 #' @examples 
@@ -139,13 +148,10 @@ locate_site <- function(site_name, format=c("id","url"), by=c("tag","dir"), ...)
 #' locate_ts("doobs", "nwis_notasite", format="url")
 #' testthat::expect_error(locate_ts("notavar", "nwis_notasite"))
 #' }
-locate_ts <- function(var_src="doobs_nwis", site_name="nwis_02322688", format=c("id","url"), by=c("tag","dir"), ...) {
+locate_ts <- function(var_src="doobs_nwis", site_name="nwis_02322688", format=c("id","url"), by=c("tag","dir","either"), limit=5000) {
   by <- match.arg(by)
   var_src <- make_ts_name(var_src) # check that the variable name is valid and add prefix
   site_name <- make_site_name(parse_site_name(site_name)) # check that the site name is reasonably valid
   format <- switch(match.arg(format), id="id", url="item_url")
-  switch(
-    by,
-    "tag"=locate_item(key=site_name, type=var_src, format=format, ...),
-    "dir"=locate_item_by_dir(parent=locate_site(site_name), title=var_src, format=format, ...))
+  locate_item(key=site_name, type=var_src, parent=locate_site(site_name), title=var_src, by=by, format=format, limit=limit)
 }
