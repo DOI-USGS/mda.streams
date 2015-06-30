96ee2d550cc59ee03d3d4e252133c529a62d5d04
Alison Appling <aappling@usgs.gov>
2015-06-30 17:25:15 -0500

    skeleton for verify_config

---
+++ R/verify_config.R
@@ -0,0 +1,10 @@
+#' Verify that a config file row has the requisite info
+#' 
+#' Checks the config row for valid src-site-logic pairs with respect to a given
+#' model
+#' 
+#' @param config a config df to verify
+#' @return TRUE if is valid, FALSE if not
+verify_config <- function(config) {
+  
+}
