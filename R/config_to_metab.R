3e1dc937dbb563bde45f6d8e67af9828326d82d2
Alison Appling <aappling@usgs.gov>
2015-07-14 14:42:37 -0500

    better error handling in config to data/metab

--- R/config_to_metab.R
+++ R/config_to_metab.R
@@ -23,8 +23,29 @@ config_to_metab <- function(config, rows, verbose=TRUE) {
     config <- read.table(config, sep="\t", header=TRUE, colClasses="character")
   }
   rows <- if(missing(rows)) 1:nrow(config) else rows
-  verify_config(config[rows,], on_fail="stop")
   
+  # Check config. If it fails verification, return right now
+  err_strs <- warn_strs <- character()
+  verification <- withCallingHandlers(
+    tryCatch({
+      verify_config(config[rows,], on_fail=stop)
+    },
+    error=function(e) { 
+      err_strs <<- paste0("verify config: ",e$message)
+      FALSE
+    }),
+    warning=function(w) { 
+      warn_strs <<- c(warn_strs, paste0("verify config: ",w$message))
+      invokeRestart("muffleWarning")
+    })
+  if(!isTRUE(verification)) {
+    out <- "config could not be verified"
+    if(length(warn_strs) > 0) attr(out, "warnings") <- warn_strs
+    if(length(err_strs) > 0) attr(out, "errors") <- err_strs
+    return(list(out)) # return a list because we're outside the lapply
+  }
+  
+  # Run metabolism for each selected config row
   fits <- lapply(rows, function(row) {
     
     if(verbose) {
@@ -32,45 +53,55 @@ config_to_metab <- function(config, rows, verbose=TRUE) {
       print(config[row,])
     }
     
-    # Locate the model function
-    metab_fun <- 
-      tryCatch(
-        # Look first in streamMetabolizer
-        get(config[row,'model'], envir = environment(streamMetabolizer::metab_model)),
-        error=function(e) {
-          # Look second in the current and inherited environments. If it's not
-          # there, either, we really do want to throw an error.
-          get(config[row,'model'])
-        }
-      )
-    # Prepare the model arguments
-    metab_args <- eval(parse(text=config[row,'model_args']))
+    # Locate the model function. Look first in streamMetabolizer, then in the
+    # current and inherited environments.
+    metab_fun <- NA
+    err_strs <- character()
+    tryCatch(metab_fun <- get(config[[row,'model']], envir = environment(streamMetabolizer::metab_model)), 
+             error=function(e) err_strs <<- append(err_strs, e$message) )
+    tryCatch(metab_fun <- get(config[[row,'model']]), 
+             error=function(e) err_strs <<- append(err_strs, e$message) )
+    if(!is.function(metab_fun)) {
+      out <- "error in locating metab_fun"
+      attr(out, "errors") <- err_strs
+      return(out)
+    }
     
-    # Prepare the data
-    metab_data <- 
-      tryCatch({
-        metab_data <- config_to_data(config[row,], row, metab_fun, metab_args)
-        metab_data <- v(metab_data) # eventually want to hot have to do this
-        metab_data[complete.cases(metab_data),]
-      },
-      error=function(e) {
-        message("failed to prepare data in row ", row, ":")
-        message(as.character(e))
-        NA
-      })
+    # Prepare the model arguments
+    metab_args <- NA
+    try(metab_args <- eval(parse(text=config[[row,'model_args']])), silent=TRUE)
+    if(isTRUE(is.na(metab_args))) {
+      out <- "error in evaluating metab_args"
+      attr(out, "errors") <- "could not parse or evaluate args"
+      return(out)
+    }
+    
+    # Prepare the data, passing along any errors from config_to_data
+    metab_data <- config_to_data(config[row,], row, metab_fun, metab_args, on_error='quiet')
+    metab_data_ok <- is.null(attr(metab_data, "errors"))
+    if(!metab_data_ok) {
+      out <- "error in data prep"
+      attr(out, "errors") <- attr(metab_data, "errors")
+      attr(out, "warnings") <- attr(metab_data, "warnings")
+      return(out)
+    } else {
+      # if the data are valid, also remove units and rows with NAs. eventually
+      # want to be able to pass units to metab_fun
+      metab_data <- v(metab_data)
+      metab_data[complete.cases(metab_data),]
+    }
     
     # Run the model
-    fit <- 
-      tryCatch({
-        if(isTRUE(is.na(metab_data))) stop("without data, won't try modeling metabolism")
-        do.call(metab_fun, c(list(data=metab_data), metab_args))
-      },
-      error=function(e) {
-        message("the metabolism model failed in row ", row, ":")
-        message(as.character(e))
-        NA
-      })
+    fit <- tryCatch({
+      do.call(metab_fun, c(list(data=metab_data), metab_args))
+    },
+    error=function(e) {
+      out <- NA
+      attr(out, "errors") <- as.character(e)
+      out
+    })
     
+    # If we've made it this far, the modeling ought to have worked
     return(fit)
   })    
     
