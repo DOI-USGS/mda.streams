437b878f53ae282110ae01eb3bfb11eba9ae4107
Alison Appling <aappling@usgs.gov>
2015-09-25 21:01:33 -0500

    record data/arg prep time in config_to_metab

--- R/config_to_metab.R
+++ R/config_to_metab.R
@@ -50,62 +50,69 @@ config_to_metab <- function(config, rows, verbose=TRUE) {
   # Run metabolism for each selected config row
   fits <- lapply(rows, function(row) {
     
-    if(verbose) message("row ", row, ": initiating metab modeling for this config row")
-    
-    # Locate the model function. Look first in streamMetabolizer, then in the
-    # current and inherited environments.
-    metab_fun <- NA
-    err_strs <- character()
-    tryCatch(metab_fun <- get(config[[row,'model']], envir = environment(streamMetabolizer::metab_model)), 
-             error=function(e) err_strs <<- append(err_strs, e$message) )
-    if(!is.function(metab_fun)) 
-      tryCatch(metab_fun <- get(config[[row,'model']]), 
+    prep_time <- system.time({
+      
+      if(verbose) message("row ", row, ": initiating metab modeling for this config row")
+      
+      # Locate the model function. Look first in streamMetabolizer, then in the
+      # current and inherited environments.
+      metab_fun <- NA
+      err_strs <- character()
+      tryCatch(metab_fun <- get(config[[row,'model']], envir = environment(streamMetabolizer::metab_model)), 
                error=function(e) err_strs <<- append(err_strs, e$message) )
-    if(!is.function(metab_fun)) {
-      out <- "error in locating metab_fun"
-      attr(out, "errors") <- err_strs
-      return(out)
-    }
-    
-    # Prepare the model arguments
-    metab_args <- NA
-    try(metab_args <- eval(parse(text=config[[row,'model_args']])), silent=TRUE)
-    if(isTRUE(is.na(metab_args)) || !is.list(metab_args)) {
-      out <- "error in evaluating metab_args"
-      attr(out, "errors") <- "could not parse or evaluate args"
-      return(out)
-    }
-    # add the config row to the info arg
-    if('info' %in% names(metab_args)) {
-      metab_args[['info']] <- list(userinfo=metab_args[['info']], config=config[row,])
-    } else {
-      metab_args <- c(metab_args, list(info=list(config=config[row,])))
-    }
-    
-    # Prepare the data, passing along any errors from config_to_data
-    if(verbose) message("row ", row, ": preparing metab_data...")
-    metab_data_list <- config_to_data(config[row,], row, metab_fun, metab_args, on_error='quiet')
-    metab_data_ok <- is.null(attr(metab_data_list, "errors"))
-    if(!metab_data_ok) {
-      out <- "error in data prep"
-      attr(out, "errors") <- attr(metab_data_list, "errors")
-      attr(out, "warnings") <- attr(metab_data_list, "warnings")
-      return(out)
-    } else {
-      # if the data are valid, also remove units and rows with NAs. eventually 
-      # want to be able to pass units to metab_fun.
-      . <- '.dplyr.var'
-      metab_data <- metab_data_list$data
-      metab_data <- u(metab_data, get_units(metab_data) %>% replace(., which(.=="mg L^-1"), "mgO2 L^-1"))
-      metab_data <- v(metab_data)
-      metab_data <- metab_data[complete.cases(metab_data),]
-      metab_data_daily <- metab_data_list$data_daily
-      if(!is.null(metab_data_daily)) {
-        metab_data_daily <- v(metab_data_daily)
-        metab_data_daily <- metab_data_daily[complete.cases(metab_data_daily),]
+      if(!is.function(metab_fun)) 
+        tryCatch(metab_fun <- get(config[[row,'model']]), 
+                 error=function(e) err_strs <<- append(err_strs, e$message) )
+      if(!is.function(metab_fun)) {
+        out <- "error in locating metab_fun"
+        attr(out, "errors") <- err_strs
+        return(out)
       }
-    }
+      
+      # Prepare the model arguments
+      metab_args <- NA
+      try(metab_args <- eval(parse(text=config[[row,'model_args']])), silent=TRUE)
+      if(isTRUE(is.na(metab_args)) || !is.list(metab_args)) {
+        out <- "error in evaluating metab_args"
+        attr(out, "errors") <- "could not parse or evaluate args"
+        return(out)
+      }
+      # add the config row to the info arg
+      if('info' %in% names(metab_args)) {
+        metab_args[['info']] <- list(userinfo=metab_args[['info']], config=config[row,])
+      } else {
+        metab_args <- c(metab_args, list(info=list(config=config[row,])))
+      }
+      
+      # Prepare the data, passing along any errors from config_to_data
+      if(verbose) message("row ", row, ": preparing metab_data...")
+      metab_data_list <- config_to_data(config[row,], row, metab_fun, metab_args, on_error='quiet')
+      metab_data_ok <- is.null(attr(metab_data_list, "errors"))
+      if(!metab_data_ok) {
+        out <- "error in data prep"
+        attr(out, "errors") <- attr(metab_data_list, "errors")
+        attr(out, "warnings") <- attr(metab_data_list, "warnings")
+        return(out)
+      } else {
+        # if the data are valid, also remove units and rows with NAs. eventually 
+        # want to be able to pass units to metab_fun.
+        . <- '.dplyr.var'
+        metab_data <- metab_data_list$data
+        metab_data <- u(metab_data, get_units(metab_data) %>% replace(., which(.=="mg L^-1"), "mgO2 L^-1"))
+        metab_data <- v(metab_data)
+        metab_data <- metab_data[complete.cases(metab_data),]
+        metab_data_daily <- metab_data_list$data_daily
+        if(!is.null(metab_data_daily)) {
+          metab_data_daily <- v(metab_data_daily)
+          metab_data_daily <- metab_data_daily[complete.cases(metab_data_daily),]
+        }
+      }
+      
+    })
     
+    # Add info on the time we took to prepare the args & data
+    metab_args$info <- c(metab_args$info, list(prep_time=prep_time))
+
     # Run the model
     if(verbose) message("row ", row, ": running metab_fun...")
     fit <- tryCatch({
