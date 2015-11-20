c130818f92f47e46e8bc2131563ea84b802f09c5
Alison Appling <aappling@usgs.gov>
2015-11-20 16:34:30 -0600

    remove bandaid for USGS-R/sbtools#140

--- R/list_metab_models.R
+++ R/list_metab_models.R
@@ -7,17 +7,15 @@ list_metab_models = function() {
   
   if(is.null(current_session()) || !session_validate()) stop("need ScienceBase access; call login_sb() first")
   
-  # get list of site items, then filter to those of the proper data_type. the 
-  # repetition is here to protect against a known bug in ScienceBase as of 
-  # 11/17/15 where paging through results doesn't work perfectly. see
-  # https://github.com/USGS-R/sbtools/issues/140
-  model_queries <- lapply(c(700,900,800,1000), function(pgsize) {
-    query_item_identifier(scheme = get_scheme(), type = 'metab_model', limit = 10000, pagesize = pgsize)$title
-  })
+  # get list of site items, then filter to those of the proper data_type
+  model_query <- query_item_identifier(scheme = get_scheme(), type = 'metab_model', limit = 10000, pagesize = 500)$title
   
-  if(length(model_queries[[1]]) > 0) {
-    model_items <- unique(do.call(c, model_queries))
-    if(length(model_items) != length(model_queries[[1]])) warning("failed to retrieve all metab models; a retry might work")
+  if(length(model_query) > 0) {
+    # check unique vs total in case an old SB bug comes back (there was
+    # duplication & omission of items when paging through many results)
+    model_items <- unique(model_query)
+    if(length(model_items) != length(model_query)) warning("failed to retrieve all metab models; a retry might work")
+    # return
     return(model_items)
   } else {
     return(character())
