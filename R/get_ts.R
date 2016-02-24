5b76fe5f760456d3f77d76c6cc457169169e6ba5
Lindsay Carr <lcarr@usgs.gov>
2016-02-24 15:37:57 -0600

    get_ts: expand_on arg added + warning for diff resolutions

--- R/get_ts.R
+++ R/get_ts.R
@@ -17,8 +17,9 @@
 #' @inheritParams combine_ts
 #' @inheritParams read_ts
 #' @export
-get_ts <- function(var_src, site_name, method='approx', approx_tol=as.difftime(3, units="hours"), on_local_exists='skip', on_invalid='stop') {
-  
+get_ts <- function(var_src, site_name, method='approx', approx_tol=as.difftime(3, units="hours"), 
+                   on_local_exists='skip', on_invalid='stop', expand_on = "leftmost") {
+
   if(length(site_name) > 1) stop("only one site_name is allowed")
   
   data_list <- lapply(var_src, function(vs) {
@@ -27,7 +28,32 @@ get_ts <- function(var_src, site_name, method='approx', approx_tol=as.difftime(3
   })
   
   if(length(var_src) > 1) {
-    combo <- do.call(combine_ts, c(data_list, list(method=method, approx_tol=approx_tol)))
+    
+    df_order <- c("DateTime", gsub("\\_.*","",var_src))
+    
+    if(expand_on == "leftmost"){
+      var_index <- 1
+      data_list_ordered <- data_list
+    } else {
+      var_index <- which(var_src == expand_on)
+      not_var_index <- which(var_src != expand_on)
+      data_list_ordered <- data_list[c(var_index, not_var_index)]
+    }
+    
+    num_obs <- unlist(lapply(data_list, nrow))
+    longer_than_var <- which(num_obs > num_obs[var_index])
+    if(any(longer_than_var)){
+      resolution_msg <- paste0("Results reflect the resolution of the ",
+                               var_src[var_index], 
+                               " timeseries. Some variables (", 
+                               paste(var_src[longer_than_var], collapse=", "),
+                               ") will lose resolution. Consider setting expand_on to set the preferred resolution or condense_stat to set the statistic used when resolution is lost.")
+      warning(resolution_msg)
+    } 
+      
+    combo <- do.call(combine_ts, c(data_list_ordered, list(method=method, approx_tol=approx_tol)))
+    combo <- combo[, df_order]
+    
   } else {
     combo <- data_list[[1]]
   }
