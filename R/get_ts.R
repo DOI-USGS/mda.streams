396d8126818e974c1f4b7205b8c4b2a7f1062911
Alison Appling <aappling@usgs.gov>
2016-03-25 00:34:36 -0500

    update & optimize get_ts

--- R/get_ts.R
+++ R/get_ts.R
@@ -2,34 +2,35 @@
 #' 
 #' The timeseries are iteratively joined, starting by joining the second element
 #' of \code{var_src} to the first, then adding in the third, etc. This method 
-#' means you have control, not just through the \code{method} and
-#' \code{approx_tol} arguments but also through how you order the elements of
-#' \code{var_src}, with consequences for the size and contents of the resulting
+#' means you have control, not just through the \code{method} and 
+#' \code{approx_tol} arguments but also through how you order the elements of 
+#' \code{var_src}, with consequences for the size and contents of the resulting 
 #' data.frame.
 #' 
-#' Downloads each file from SB if either (1) the file has not yet been downloaded
-#' to the code{tempdir()} during this R session, or (2) 
+#' Downloads each file from SB if either (1) the file has not yet been
+#' downloaded to the code{tempdir()} during this R session, or (2) 
 #' \code{on_local_exists='replace'}. There's a small risk that the resulting ts 
 #' will be out of date relative to ScienceBase, but the benefit is faster 
 #' ts-getting.
 #' 
-#' @return 
-#' \code{get_ts} returns a data.frame, where the first column is the \code{DateTime} 
-#' and subsequent columns are the timeseries defined in \code{var_src}. The names of
-#' the variable columns are equal to the string in each \code{var_src} variable 
-#' before the underscore. E.g. if \code{var_src} had a variable \code{"disch_nwis"}, 
-#' then the corresponding column name would be \code{disch}. 
+#' @return \code{get_ts} returns a data.frame, where the first column is the
+#' \code{DateTime} and subsequent columns are the timeseries defined in
+#' \code{var_src}. The names of the variable columns are equal to the string in
+#' each \code{var_src} variable before the underscore. E.g. if \code{var_src}
+#' had a variable \code{"disch_nwis"}, then the corresponding column name would
+#' be \code{disch}.
 #' 
 #' @param match_var character string indicating which variable's timesteps the 
-#'   resulting data.frame should match. The string must also be in `var_src`. The  
-#'   default chooses the first variable listed in `var_src`. 
+#'   resulting data.frame should match. The string must also be in `var_src`.
+#'   The default chooses the first variable listed in `var_src`.
 #' @param condense_stat function name used to condense observations to 
-#'   `match_var`'s timestep (only for variables with more frequent observations than 
-#'   `match_var`), or the term `match` to indicate that the function defined in `method`
-#'   will be used to match the timestep of `match_var`. Function names should be 
-#'   unquoted, where as `match` should be string. Examples of what to use: mean (default), 
-#'   median, max, and min. A custom function can also be used, but it's input must be 
-#'   a numeric vector and output must be a single numeric value. 
+#'   `match_var`'s timestep (only for variables with more frequent observations
+#'   than `match_var`), or the term `match` to indicate that the function
+#'   defined in `method` will be used to match the timestep of `match_var`.
+#'   Function names should be unquoted, where as `match` should be string.
+#'   Examples of what to use: mean (default), median, max, and min. A custom
+#'   function can also be used, but it's input must be a numeric vector and
+#'   output must be a single numeric value.
 #' @param day_start start time (inclusive) of a day's data in number of hours 
 #'   from the midnight that begins the date. For example, day_start=-1.5 
 #'   indicates that data describing 2006-06-26 begin at 2006-06-25 22:30, or at 
@@ -46,14 +47,16 @@
 #'   the midnight that begins the date. For example, day_end=30 indicates that 
 #'   data describing 2006-06-26 end at the last observation time that occurs 
 #'   before 2006-06-27 06:00. See day_start for recommended start and end times.
-#' 
+#' @param quietly logical. if one or more timeseries will be truncated, padded
+#'   with NAs, or condensed, should a warning message be given?
+#'   
 #' @inheritParams download_ts
 #' @inheritParams combine_ts
 #' @inheritParams read_ts
 #' @export
 get_ts <- function(var_src, site_name, method='approx', approx_tol=as.difftime(3, units="hours"), 
                    on_local_exists='skip', on_invalid='stop', match_var = "leftmost", 
-                   condense_stat = mean, day_start = 4, day_end = 28) {
+                   condense_stat = mean, day_start = 4, day_end = 28, quietly=FALSE) {
 
   if(length(site_name) > 1) stop("only one site_name is allowed")
   if(length(match_var) > 1) stop("only one match_var is allowed")
@@ -76,14 +79,10 @@ get_ts <- function(var_src, site_name, method='approx', approx_tol=as.difftime(3
     data_list_ordered <- data_list[c(var_index, not_var_index)] #ordered with match_var on far left for use in combine_ts
     var_src_ordered <- var_src[c(var_index, not_var_index)]
 
-    if(condense_stat == "match"){
-      condense_stat_nm <- condense_stat
-    } else { 
-      condense_stat_nm <-as.character(substitute(condense_stat))[1] 
-      if(condense_stat_nm == 'function'){condense_stat_nm <- 'custom function'} 
-    }
+    condense_stat_nm <- as.character(substitute(condense_stat))[1] 
+    if(condense_stat_nm == 'function'){condense_stat_nm <- 'custom function'} 
     
-    warning_info <- warning_table(var_src_ordered, condense_stat_nm, data_list_ordered, site_name, method)
+    warning_info <- warning_table(var_src_ordered, condense_stat_nm, data_list_ordered, site_name, method, quietly)
 
     # applying condense_stat
     to_condense <- grep("Condensed", warning_info$result)
@@ -93,17 +92,25 @@ get_ts <- function(var_src, site_name, method='approx', approx_tol=as.difftime(3
                         NA)
     
     #distinguish NA observations from NAs added during the full_join
-    original_obs_list <- lapply(data_list_ordered[to_condense], function(d){
-      nm <- paste0("has.", names(d)[2])
-      d[[nm]] <- TRUE
-      return(d)
-    })
-    original_obs_list$by <- "DateTime"
-    to_condense_df <- do.call("full_join", original_obs_list)
-    
-    vars_condensed_list <- condense_by_stat(to_condense_df, condense_stat = condense_stat, site_lon = longitude, 
-                          day_start = day_start, day_end = day_end)
-    data_list_condensed <- append(data_list_ordered[-to_condense], vars_condensed_list)
+    if(length(to_condense) > 0) {
+      original_obs_list <- lapply(data_list_ordered[to_condense], function(d){
+        nm <- paste0("has.", names(d)[2])
+        d[[nm]] <- TRUE
+        return(d)
+      })
+      if(length(to_condense) > 1) {
+        to_condense_df <- do.call("full_join", c(original_obs_list, list(by='DateTime'))) %>%
+          {.[order(.$DateTime),]}
+      } else {
+        to_condense_df <- original_obs_list[[1]]
+      }
+      
+      vars_condensed_list <- condense_by_stat(to_condense_df, condense_stat = condense_stat, site_lon = longitude, 
+                                              day_start = day_start, day_end = day_end)
+      data_list_condensed <- append(data_list_ordered[-to_condense], vars_condensed_list)
+    } else {
+      data_list_condensed <- data_list_ordered
+    }
     
     combo <- do.call(combine_ts, c(data_list_condensed, list(method=method, approx_tol=approx_tol)))
     combo <- combo[, df_order] #put columns back as user specified
@@ -114,20 +121,29 @@ get_ts <- function(var_src, site_name, method='approx', approx_tol=as.difftime(3
   combo
 }
 
+#' Condense a data.frame to daily resolution
+#' 
+#' Cndense according to day_start, day_end, and the local solar mean time of the
+#' site
+#' 
+#' @param ts the ts dataframe to condense
+#' @inheritParams get_ts
+#' @param site_lon the longitude of the site
+#' @keywords internal
 condense_by_stat <- function(ts, condense_stat, site_lon, day_start, day_end){
     
   solar.time <- convert_UTC_to_solartime(ts$DateTime, site_lon, time.type = "mean solar")
   ts_solar <- ts %>% v() %>% mutate(solar.time = solar.time)
   
-  ts_condensed <- streamMetabolizer:::mm_model_by_ply(
+  ts_condensed <- mm_model_by_ply(
     model_fun=condense_by_ply, # should look like mm_model_by_ply_prototype
     data=ts_solar, #include sitetime
     data_daily=NULL,
     day_start=day_start,
     day_end=day_end,
-    #day_tests = c(),
-    stat_func=condense_stat
-  )
+    day_tests = c(),
+    timestep_days=FALSE,
+    stat_func=condense_stat)
   
   ts_condensed <- ts_condensed %>% 
     mutate(DateTime = convert_solartime_to_UTC(as.POSIXct(paste(as.character(date), "12:00:00"), tz='UTC'),
@@ -142,7 +158,20 @@ condense_by_stat <- function(ts, condense_stat, site_lon, day_start, day_end){
   return(ts_condensed_list)
 }
 
-condense_by_ply <- function(data_ply, data_daily_ply, ..., day_start, day_end, ply_date) {
+#' Condense function called by mm_model_by_ply
+#' 
+#' Following mm_model_by_ply_prototype
+#' 
+#' @param data_ply as in mm_model_by_ply_prototype
+#' @param data_daily_ply as in mm_model_by_ply_prototype
+#' @param day_start as in mm_model_by_ply_prototype
+#' @param day_end as in mm_model_by_ply_prototype
+#' @param ply_date as in mm_model_by_ply_prototype
+#' @param ply_validity as in mm_model_by_ply_prototype
+#' @param timestep_days as in mm_model_by_ply_prototype
+#' @param ... as in mm_model_by_ply_prototype
+#' @keywords internal
+condense_by_ply <- function(data_ply, data_daily_ply, day_start, day_end, ply_date, ply_validity, timestep_days, ...) {
   args <- list(...)
   
   dates_col <- which(names(data_ply) == "solar.time")
@@ -161,8 +190,17 @@ condense_by_ply <- function(data_ply, data_daily_ply, ..., day_start, day_end, p
   return(summarize_stat)
 }
 
-warning_table <- function(var_src, condense_stat, data, site_name, method){
-  timestep_df <- summarize_ts(var_src, site_name, out="modal_timestep") %>% unitted::v()
+#' Describe data changes that will occur on combining multiple tses
+#' 
+#' Throw a warning containing a table of resolution & extent changes when
+#' var_srces are condensed. Return a smaller table containing the timestep and
+#' expected resolution of each var_src after condensing.
+#' 
+#' @inheritParams get_ts
+#' @param data list of downloaded timeseries data.frames, as named in var_src
+#' @keywords internal
+warning_table <- function(var_src, condense_stat, data, site_name, method, quietly){
+  timestep_df <- summarize_ts(var_src=setNames(data, var_src), site_name, out="modal_timestep") %>% unitted::v()
 
   all_dates <- do.call(rbind, lapply(data, function(data) {
     all_dates <- unitted::v(data$DateTime)
@@ -182,28 +220,28 @@ warning_table <- function(var_src, condense_stat, data, site_name, method){
     if(t == t_match){
       res_result <- "As is"
     }else if(t > t_match){
-      res_result <- "NAs introduced"
+      res_result <- "NAs added"
     }else {
       if(condense_stat == "match"){
         res_result <- paste("Matched by", method)
       } else {
         if(t_match < 1440){
-          res_result <- "Matched by approx (condense_stat not supported for sub-daily)"
+          res_result <- paste("Matched by", method, "(condense_stat not supported for sub-daily)")
         } else {
           res_result <- paste("Condensed by", condense_stat)
         }
       }
     }
     
-    if(s < s_match){start_result <- "Earlier (unused data)"}
-    if(s > s_match){start_result <- "Later (missing data)"}
+    if(s < s_match){start_result <- "Earlier (truncated)"}
+    if(s > s_match){start_result <- "Later (NAs added)"}
     if(s == s_match){start_result <- "Equal"}
 
-    if(e < e_match){end_result <- "Earlier (missing data)"}
-    if(e > e_match){end_result <- "Later (unused data)"}
+    if(e < e_match){end_result <- "Earlier (NAs added)"}
+    if(e > e_match){end_result <- "Later (truncated)"}
     if(e == e_match){end_result <- "Equal"}
 
-    return(data.frame(var_src = v, resolution_change = res_words, result = res_result, start_date = start_result, 
+    return(data.frame(var_src = v, resolution_change = res_words, resolution_result = res_result, start_date = start_result, 
                       end_date = end_result, stringsAsFactors = FALSE))
   }
   
@@ -218,11 +256,19 @@ warning_table <- function(var_src, condense_stat, data, site_name, method){
   
   warning_df <- warning_df %>% 
     mutate(resolution_change = paste(resolution_change, "to", match_res)) %>% 
-    mutate(resolution_change = replace(resolution_change, result == "As is", "No change"))
+    mutate(resolution_change = replace(resolution_change, resolution_result == "As is", "No change"))
 
-  warning(paste0(capture.output(print(warning_df, row.names=FALSE)), collapse='\n'))
+  # if there are any irregularities whatsoever, throw a warning
+  if(!quietly) {
+    if(any(warning_df$resolution_change != 'No change') ||
+       any(warning_df$resolution_result != 'As is') ||
+       any(warning_df$start_date != 'Equal') ||
+       any(warning_df$end_date != 'Equal')) {
+      warning(paste0(capture.output(print(warning_df, row.names=FALSE)), collapse='\n'))
+    }
+  }
   
   return(data.frame(timestep = timestep_df$modal_timestep, 
-                    result = warning_df$result,
+                    result = warning_df$resolution_result,
                     stringsAsFactors = FALSE))
 }
---
+++ tests/testthat/test-get_ts.R
@@ -0,0 +1,43 @@
+context('get_ts')
+
+test_that('get_ts works', {
+  
+  # easy ones: no errors or warnings
+  doobs <- get_ts('doobs_nwis', 'nwis_06601200')
+  sw <- get_ts('sw_nldas', 'nwis_06601200')
+  gpp <- get_ts('gpp_estBest', 'nwis_06601200')
+  
+  # multiple sites gives error on purpose
+  expect_error(get_ts('sitetime_calcLon', c('nwis_06601200','nwis_08062500')), "only one site_name is allowed")
+  
+  # multiple compatible tses works fine, no warning or error
+  tses <- get_ts(c('sitetime_calcLon','doobs_nwis'), 'nwis_06601200')
+  
+  # multiple different-length, different-resolution tses give useful warnings
+  expect_warning(get_ts(c('sw_nldas','doobs_nwis'), 'nwis_06601200'), 'Matched by approx')
+  expect_warning(get_ts(c('sw_nldas','doobs_nwis'), 'nwis_06601200', method='full_join'), 'Matched by full_join')
+  hourlytohalf1 <- tryCatch(get_ts(c('doobs_nwis','sw_nldas'), 'nwis_06601200'), warning=function(w) w$message)
+  hourlytohalf2 <- tryCatch(get_ts(c('sw_nldas','doobs_nwis'), 'nwis_06601200', match_var='doobs_nwis'), warning=function(w) w$message)
+  expect_equal(hourlytohalf1, hourlytohalf2)
+  
+  # multiple different-length, different-resolution tses give expecteed dims
+  matchsw <- get_ts(c('sw_nldas','doobs_nwis'), 'nwis_06601200', quietly=TRUE)
+  matchdoobs1 <- get_ts(c('doobs_nwis','sw_nldas'), 'nwis_06601200', quietly=TRUE)
+  matchdoobs2 <- get_ts(c('sw_nldas','doobs_nwis'), match_var='doobs_nwis', 'nwis_06601200', quietly=TRUE)
+  expect_equal(nrow(matchsw), nrow(sw))
+  expect_equal(nrow(matchdoobs1), nrow(doobs))
+  expect_equal(nrow(matchdoobs2), nrow(doobs))
+  
+  # realistic request
+  tses1 <- get_ts(c('gpp_estBest','doobs_nwis','doamp_calcDAmp'), 'nwis_06601200', quietly=TRUE) # one var to condense
+  profvis::profvis(get_ts(c('gpp_estBest','doobs_nwis','doamp_calcDAmp','baro_nldas'), 'nwis_06601200', quietly=TRUE)) # >1 var to condense
+  expect_equal(nrow(tses1), nrow(gpp))
+  expect_equal(nrow(tses2), nrow(gpp))
+  
+  # working code to profile code & make it faster - most of the work had to be done in summarize_ts  
+  # devtools::install_github("rstudio/profvis")
+  # library(profvis)
+  # profvis(tses <- get_ts(c('sitetime_calcLon','doobs_nwis'), 'nwis_06601200')) #92% in summarize_ts
+  # profvis(tssum <- summarize_ts(rep(c("doobs_nwis", "wtr_nwis"), each=4), rep(c("nwis_01021050","nwis_01036390","nwis_01073389","nwis_notasite"), times=2), out='modal_timestep'))
+  
+})
