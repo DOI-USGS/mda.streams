d3e8b9c43347b8e9a386bd1ca68e3661f800deea
Alison Appling <aappling@usgs.gov>
2016-07-04 20:15:11 -0500

    beef up get_site_coords to also return alt (optional, not the default, but useful for stage_calc_ts)

--- R/get_site_coords.R
+++ R/get_site_coords.R
@@ -26,13 +26,13 @@
 #' get_site_coords(c("styx_001001","nwis_07239450"), use_basedon=TRUE) 
 #' get_site_coords(c("nwis_01467200","nwis_09327000","nwis_351111089512501",
 #'     "styx_000001001"), format="geoknife")
-get_site_coords <- function(site_names, format=c("normal","geoknife"), on_missing=c("NA","omit"), use_basedon=FALSE, attach.units=(format=="normal")) {
+get_site_coords <- function(site_names, format=c("normal","geoknife"), on_missing=c("NA","omit"), out=c('site_name','lat','lon'), use_basedon=FALSE, attach.units=(format=="normal")) {
 
   format <- match.arg(format)
   on_missing <- match.arg(on_missing)
     
   # retrieve the coordinates from meta_basic
-  basic_meta <- get_meta('basic', out=c('site_name','lat','lon'))
+  basic_meta <- get_meta('basic', out=union(c('site_name','lat','lon'), out))
   ll_row <- match(site_names, basic_meta$site_name)
   if(length(na_rows <- which(is.na(ll_row))) > 0) {
     missing_site_info <- paste0(" unrecognized site_name[s]: ", paste0(site_names[na_rows], collapse=", "))
@@ -47,16 +47,18 @@ get_site_coords <- function(site_names, format=c("normal","geoknife"), on_missin
     warning(on_missing_info, missing_site_info)
   }
   lon_lat <- basic_meta[ll_row,]
-  lon_lat$site_name <- site_names
+  #lon_lat$site_name <- site_names
   
-  # patch NAs for sites with proxies (basedons) where we can
+  # patch NAs for sites with proxies (basedons) for sites where ALL values 
+  # besides site_name are NA and where a proxy is available
   if(use_basedon) {
-    na_lat_lons <- with(lon_lat, which(is.na(lat) | is.na(lon)))
+    na_lat_lons <- which(sapply(1:nrow(lon_lat), function(i) all(is.na(lon_lat[i, setdiff(out, 'site_name')]))))
     if(length(na_lat_lons) > 0) {
       styx_meta <- get_meta("styx")
       sites_w_proxies <- match(lon_lat$site_name[na_lat_lons], styx_meta$site_name)
       proxies <- styx_meta[sites_w_proxies, 'styx.basedon']
-      lon_lat[na_lat_lons, c('lat','lon')] <- basic_meta[match(proxies, basic_meta$site_name),c('lat','lon')]
+      non_site_cols <- setdiff(out, 'site_name')
+      lon_lat[na_lat_lons, non_site_cols] <- basic_meta[match(proxies, basic_meta$site_name), non_site_cols]
     }
   }
   
@@ -64,6 +66,9 @@ get_site_coords <- function(site_names, format=c("normal","geoknife"), on_missin
     lon_lat <- v(lon_lat)[c("lon","lat")] %>%
       t() %>% as.data.frame() %>% setNames(lon_lat$site_name)
     if(attach.units) stop("sorry - can't attach units to geoknife format")
+  } else {
+    # if not geoknife foramt, honor the selection for 'out'
+    lon_lat <- lon_lat[,out]
   }
   
   if(!attach.units) {
